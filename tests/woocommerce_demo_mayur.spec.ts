be8a19c21f1ccd906d2da692f5995d668bba59a3
t3sting-qa <t3stingqa@gmail.com>
2025-07-10 15:28:41 +0530

    Update woocommerce_demo_mayur.spec.ts
    test 2 responsiveness

--- tests/woocommerce_demo_mayur.spec.ts
+++ tests/woocommerce_demo_mayur.spec.ts
@@ -2,6 +2,21 @@ import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 
 test('Test Onlineshop1 Home Page', async ({ page }) => {
-  await page.goto('https://mustershop-baiersdorf.de/');
+  await page.goto('https://mustershop-baiersdorf.de');
   await percySnapshot(page, 'Onlineshop1 Home Page');
+
+test('Onlineshop1 Home Responsive', async ({ page }) => {
+  await page.goto('https://mustershop-baiersdorf.de');
+
+  // Desktop
+  await page.setViewportSize({ width: 1280, height: 800 });
+  await percySnapshot(page, 'Home - Desktop');
+
+  // Tablet
+  await page.setViewportSize({ width: 768, height: 1024 });
+  await percySnapshot(page, 'Home - Tablet');
+
+  // Mobile
+  await page.setViewportSize({ width: 375, height: 667 });
+  await percySnapshot(page, 'Home - Mobile');
 });
