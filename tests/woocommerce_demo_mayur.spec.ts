cfd33f8be49bac94490105ef101a1cfbcf4589d1
t3sting-qa <t3stingqa@gmail.com>
2025-07-10 15:45:45 +0530

    Update woocommerce_demo_mayur.spec.ts
    finalize responsiveness test

--- tests/woocommerce_demo_mayur.spec.ts
+++ tests/woocommerce_demo_mayur.spec.ts
@@ -3,14 +3,13 @@ import percySnapshot from '@percy/playwright';
 
 test('Test Onlineshop1 Home Page Responsiveness', async ({ page }) => {
   await page.goto('https://mustershop-baiersdorf.de');
-  await percySnapshot(page, 'Onlineshop1 Home Page');
-
+  
   // Desktop
   await page.setViewportSize({ width: 1280, height: 800 });
   await percySnapshot(page, 'Home - Desktop');
 
   // Tablet
-  await page.setViewportSize({ width: 768, height: 1024 });
+  await page.setViewportSize({ width: 1024, height: 768 });
   await percySnapshot(page, 'Home - Tablet');
 
   // Mobile
