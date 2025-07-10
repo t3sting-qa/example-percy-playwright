4b54fddb32b5fb4a96fd17ef7ee26a1208319880
t3sting-qa <t3stingqa@gmail.com>
2025-07-10 15:13:50 +0530

    Update woocommerce_demo_mayur.spec.ts
    onlineshop homepage

--- tests/woocommerce_demo_mayur.spec.ts
+++ tests/woocommerce_demo_mayur.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 
-test('Test Google Home Page', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await percySnapshot(page, 'Google Home Page');
+test('Test Onlineshop1 Home Page', async ({ page }) => {
+  await page.goto('https://mustershop-baiersdorf.de/');
+  await percySnapshot(page, 'Onlineshop1 Home Page');
 });
