fc356d9b8a953676d6a4fdea06481a0ac1e7dfb4
t3sting-qa <t3stingqa@gmail.com>
2025-07-10 16:07:23 +0530

    Update woocommerce_demo_mayur.spec.ts
    expect import

--- tests/woocommerce_demo_mayur.spec.ts
+++ tests/woocommerce_demo_mayur.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 
 test('Test Onlineshop1 Home Page Responsiveness', async ({ page }) => {
